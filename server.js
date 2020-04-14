f3cddfcfcf716c0daa9ddd8c5426b77e26cf05e5
Amber Wilson <678866+amberwilson@users.noreply.github.com>
2020-04-14 20:57:33 -0300

    When POSTing, add createdAt and set id to a cuid

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const jsonServer = require('json-server');
+const cuid = require('cuid');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || process.argv.slice(2)[0] || 3000;
 
 server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+// Parse body so that middleware can access payloads
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+    req.body.id = cuid().toString();
+  }
+
+  // Continue to JSON Server router
+  next();
+});
+
+server.use(router);
+server.listen(port, () => {
+  console.log(`json-server API started on port ${port}`);
+});
